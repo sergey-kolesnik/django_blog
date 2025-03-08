961df38f3c5bf910793154f247a4d74c61dd6261
serg <serg@localhost.localdomain>
2025-03-08 18:22:21 +0300

    update urls.py, models.py, create templates base,list,detail.html

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 
 
@@ -39,7 +40,10 @@ class Post(models.Model):
 
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(
+        max_length=250,
+        unique_for_date="publish",
+        )
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
@@ -74,3 +78,10 @@ class Post(models.Model):
     def __str__(self):
         """Возвращает заголовок поста."""
         return self.title
+    
+
+    def get_absolute_url(self):
+        return reverse(
+            "blog:post_detail",
+            args=[self.id],
+        )
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,8 @@
 from django.http import Http404
-from django.shortcuts import render, get_object_or_404,
+from django.shortcuts import (
+    render, 
+    get_object_or_404
+    )
 
 from .models import Post
 
